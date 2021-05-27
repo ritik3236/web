b58459e2ba21dafd36f4b3cfe534561b0c5784f4
Pranjal Kushwaha <pranjal0819@gmail.com>
2021-05-28 00:58:24 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import re
 import sys
+
+
+def read_env():
+    """ Reads local default environment variables from a .env file
+    located in the project root directory.
+    https://gist.github.com/bennylope/2999704
+    """
+    try:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')) as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
+
 
 
 def main():
@@ -17,6 +43,7 @@ def main():
         ) from exc
         # Run Virtual Envoirnment if getting error invalid syntax from exc
         # run to activate Virtual Envoirnment : source <venv>/bin/acivate
+    read_env()
     execute_from_command_line(sys.argv)
 
 
